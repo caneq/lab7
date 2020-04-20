684011aef03865fbe9501e6f6c533a9e59e58943
Korobkin Alexander <korobkin.alex2000@yandex.ru>
2020-04-20 23:22:22 +0300

    implement servers main

--- src/server/Main.java
+++ src/server/Main.java
@@ -1,7 +1,38 @@
 package server;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
 public class Main {
+    private static final int PORT = 775;
+    UsersHandler usersHandler  = new UsersHandler();
+
     public static void main(String[] args){
         System.out.println("Hello world!");
+        new Main().run();
+        System.out.println("server GG");
     }
+
+    public Main() {}
+
+    public void run(){
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    new UserThread(socket, usersHandler);
+
+                } catch (IOException exc) {
+                    exc.printStackTrace();
+                }
+            }
+        } catch (IOException exc) {
+            System.out.println("server GG");
+            exc.printStackTrace();
+        }
+    }
+
 }
