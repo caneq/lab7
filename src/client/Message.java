971c1f5d0b7e169d42861726df0bfcdfdb8ea451
Korobkin Alexander <korobkin.alex2000@yandex.ru>
2020-04-20 18:20:18 +0300

    start

---
+++ src/client/Message.java
@@ -0,0 +1,12 @@
+package client;
+
+public class Message {
+    public String sender;
+    public String message;
+
+    public Message(String sender, String message){
+        this.sender = sender;
+        this.message = message;
+    }
+
+}
